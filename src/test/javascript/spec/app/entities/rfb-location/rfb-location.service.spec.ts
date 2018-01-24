dfdce6067f2f856a4e58e2e54873dbb658c147c8
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2018-01-23 22:40:22 -0200

    Adding JPA entities

---
+++ src/test/javascript/spec/app/entities/rfb-location/rfb-location.service.spec.ts
@@ -0,0 +1,79 @@
+/* tslint:disable max-line-length */
+import { TestBed, async } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { ConnectionBackend, RequestOptions, BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
+import { JhiDateUtils } from 'ng-jhipster';
+
+import { RfbLocationService } from '../../../../../../main/webapp/app/entities/rfb-location/rfb-location.service';
+import { RfbLocation } from '../../../../../../main/webapp/app/entities/rfb-location/rfb-location.model';
+
+describe('Service Tests', () => {
+
+    describe('RfbLocation Service', () => {
+        let service: RfbLocationService;
+
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                providers: [
+                    {
+                        provide: ConnectionBackend,
+                        useClass: MockBackend
+                    },
+                    {
+                        provide: RequestOptions,
+                        useClass: BaseRequestOptions
+                    },
+                    Http,
+                    JhiDateUtils,
+                    RfbLocationService
+                ]
+            });
+
+            service = TestBed.get(RfbLocationService);
+
+            this.backend = TestBed.get(ConnectionBackend) as MockBackend;
+            this.backend.connections.subscribe((connection: any) => {
+                this.lastConnection = connection;
+            });
+        }));
+
+        describe('Service methods', () => {
+            it('should call correct URL', () => {
+                service.find(123).subscribe(() => {});
+
+                expect(this.lastConnection).toBeDefined();
+                expect(this.lastConnection.request.url).toEqual('api/rfb-locations/' + 123);
+            });
+            it('should return RfbLocation', () => {
+
+                let entity: RfbLocation;
+                service.find(123).subscribe((_entity: RfbLocation) => {
+                    entity = _entity;
+                });
+
+                this.lastConnection.mockRespond(new Response(new ResponseOptions({
+                    body: JSON.stringify({id: 123}),
+                })));
+
+                expect(entity).toBeDefined();
+                expect(entity.id).toEqual(123);
+            });
+
+            it('should propagate not found response', () => {
+
+                let error: any;
+                service.find(123).subscribe(null, (_error: any) => {
+                    error = _error;
+                });
+
+                this.lastConnection.mockError(new Response(new ResponseOptions({
+                    status: 404,
+                })));
+
+                expect(error).toBeDefined();
+                expect(error.status).toEqual(404);
+            });
+        });
+    });
+
+});
